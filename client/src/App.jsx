8e0ff05a17c7223a7d12ca5c6e1adc6cc59eba70
Maziar Pedrami <awildroot@gmail.com>
2023-12-03 18:54:48 +0100

    created frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Properties from '../src/component/Properties'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <Properties/>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/component/Properties/index.jsx
@@ -0,0 +1,9 @@
+
+
+export default function index() {
+  return (
+    <div>
+      <h1>List of Properties</h1>      
+    </div>
+  )
+}
